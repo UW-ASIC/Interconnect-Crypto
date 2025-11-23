05178a750bebe55b30a5b552e60053c54363b5f3
Peri Gandhi <pvgandhi@uwaterloo.ca>
2025-11-22 22:01:53 -0500

    it is 10 PM...

--- src/data_bus.v
+++ src/data_bus.v
@@ -42,10 +42,6 @@ module data_bus(
 
 // --- Sending logic ---
     always @(*) begin
-        send_ready         = 0;       // assign default
-        first_pkt_received = first_pkt_received; // preserve sequential state
-        read_address       = 0;       // default
-        ownership          = ownership; // preserve sequential state
 
         if (!rst_n) begin
             ownership           = 0;
@@ -54,10 +50,6 @@ module data_bus(
             read_address        = 0;
 
         end else begin
-            ownership          = ownership;      // or 0 if you want default low
-            send_ready         = 0;
-            first_pkt_received  = first_pkt_received; // or 0
-            read_address       = 0;
 
             if(ack) begin 
                 first_pkt_received  = 0;
@@ -101,10 +93,13 @@ module data_bus(
                     end else begin
                         ownership = 1;
                         send_ready = 1;
-                    end
+                    end 
+                end else begin 
+                    send_ready = 0;
+                    ownership = 0;
                 end 
             end
-        end
+        end 
     end
 
     // --- Receiving logic ---
@@ -121,41 +116,40 @@ module data_bus(
 
         end else begin
 
-            recv_valid = 0;
-            recv_data  = 0;
-            bus_ready  = 1;
-
             if (ack) begin 
                 allowed_source = 7;
                 allowed_dest = 7;
-            end 
+                recv_valid = 0;
 
-            if (bus_valid == 1'b1) begin
+            end else begin
 
-                // Grab src and dest from first packet
-                if (read_address && !ack) begin
-                    allowed_source      = {1'b0, bus_data[3:2]};
-                    allowed_dest        = {1'b0, bus_data[5:4]};
-                end
+                if (bus_valid == 1'b1) begin
 
-                // Only allow reading if this module is source or destination
-                if ({1'b0, source_id} == allowed_source || source_id == 2'b11 || {1'b0, source_id} == allowed_dest) begin
-                    recv_valid = 1;
-                    recv_data  = bus_data;
-                    bus_ready  = 1;
+                    // Grab src and dest from first packet
+                    if (read_address && !ack) begin
+                        allowed_source      = {1'b0, bus_data[3:2]};
+                        allowed_dest        = {1'b0, bus_data[5:4]};
+                    end
 
+                    // Only allow reading if this module is source or destination
+                    if ({1'b0, source_id} == allowed_source || source_id == 2'b11 || {1'b0, source_id} == allowed_dest) begin
+                        recv_valid = 1;
+                        recv_data  = bus_data;
+                        bus_ready  = 1;
+
+
+                    end else begin
+                        recv_valid = 0;
+                        recv_data  = 0;
+                        bus_ready  = 0;
+                    end
 
                 end else begin
                     recv_valid = 0;
                     recv_data  = 0;
-                    bus_ready  = 0;
                 end
-
-            end else begin
-                recv_valid = 0;
-                recv_data  = 0;
             end
-        end
+        end 
     end
 
 
