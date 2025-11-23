f6c7c9d72f4d74e26cdede455e98c3cc8d0dbd77
Peri Gandhi <pvgandhi@uwaterloo.ca>
2025-11-22 21:16:28 -0500

    test purpose

--- src/data_bus.v
+++ src/data_bus.v
@@ -53,25 +53,53 @@ always @(posedge clk or negedge rst_n) begin
     end
 end
 
+always @(posedge clk or negedge rst_n) begin 
+    if (!rst_n) begin
+        first_pkt_received <= 0;
+        ownership <= 0;
+    end else if (ack) begin 
+        ownership           <= 0;    
+        first_pkt_received  <= 0;
+    end else begin 
+
+        if(bus_valid && !first_pkt_received) begin 
+            first_pkt_received  <= 1;
+        end 
+
+        if(source_id == 2'b11 && send_valid) begin
+            ownership <= 1;
+        end
+
+        if ((source_id == allowed_source[1:0]) && bus_valid && first_pkt_received) begin
+            if(i <= 3) begin 
+                ownership <= 1;
+            end
+        end
+
+
+
+    end
+end 
+
 // --- Sending logic ---
     always @(*) begin
         send_ready         = 0;       // assign default
-        first_pkt_received = first_pkt_received; // preserve sequential state
+        // first_pkt_received = first_pkt_received; // preserve sequential state
         read_address       = 0;       // default
-        ownership          = ownership; // preserve sequential state
+        // ownership          = ownership; // preserve sequential state
 
         if (!rst_n) begin
-            ownership           = 0;
+            // ownership           = 0;
             send_ready          = 1; // Default High on Reset
-            first_pkt_received  = 0;
+            // first_pkt_received  = 0;
             // i                   = 0;
             read_address        = 0;
 
         end else begin
             if(ack) begin 
-                first_pkt_received  = 0;
+                // first_pkt_received  = 0;
                 send_ready          = 1;
-                ownership           = 0;    
+                // ownership           = 0;    
                 // i                   = 0;
                 read_address        = 0;
 
@@ -80,7 +108,7 @@ end
                 // PART 1: UPDATE INTERNAL FLAGS (Run this independently)
                 // ==========================================================
                 if(bus_valid && !first_pkt_received) begin 
-                    first_pkt_received  = 1;
+                    // first_pkt_received  = 1;
                     read_address        = 1; 
                 end else if (bus_valid && first_pkt_received) begin
                     read_address        = 0;
@@ -90,7 +118,7 @@ end
                 // PART 2: HANDLE OWNERSHIP & READY (Run this independently)
                 // ==========================================================
                 if(source_id == 2'b11 && send_valid) begin
-                    ownership = 1;
+                    // ownership = 1;
                     send_ready = 1; // MUST be 1 immediately
                 end
 
@@ -110,7 +138,7 @@ end
                         // i = i + 1;
                         send_ready = 0; 
                     end else begin
-                        ownership = 1;
+                        // ownership = 1;
                         send_ready = 1;
                     end
                 end 
@@ -128,24 +156,11 @@ end
         if (!rst_n) begin
             recv_valid           = 0;
             recv_data            = 0;
-            allowed_source       = 0;
-            allowed_dest         = 0;
             bus_ready            = 1;
 
         end else begin
 
-            if (ack) begin 
-                allowed_source = 7;
-                allowed_dest = 7;
-            end 
-
             if (bus_valid == 1'b1) begin
-
-                // Grab src and dest from first packet
-                if (read_address && !ack) begin
-                    allowed_source      = {1'b0, bus_data[3:2]};
-                    allowed_dest        = {1'b0, bus_data[5:4]};
-                end
 
                 // Only allow reading if this module is source or destination
                 if (source_id == allowed_source[1:0] || source_id == 2'b11 || source_id == allowed_dest[1:0]) begin
