9e4969c3fb971290fc089b08804bbdcc9e084e5f
Bob Li <bobli8203@gmail.com>
2026-02-01 21:42:47 -0500

    Added test for ready signal

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge
 
 
 @cocotb.test()
@@ -38,3 +38,58 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+
+@cocotb.test()
+async def ready_test(dut):
+    
+    dut._log_info("Reset")
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10, unit = "us")
+    dut.rst_n.value = 1
+    dut._log.info("Testing Ready Signals")
+
+    #initially, set all ready values to 0, this means that all the values are not ready
+    dut.mem_ready.value = 0
+    dut.sha_ready.value = 0
+    dut.aes_ready.value = 0
+    dut.ctrl_ready.value = 0
+    dut._log.info("All infos not set to ready")
+    #wait for rising edge and check if the bus is not ready
+    await RisingEdge(dut.clk)
+    assert dut.bus_ready.value == 0
+    dut._log.info("Passed all not ready test")
+
+    #now set the mem to be ready
+    dut.mem_ready.value = 1
+    dut._log.info("mem is set to ready")
+    #wait for rising edge to check
+    await RisingEdge(dut.clk)
+    assert dut.bus_ready.value == 1
+
+    #reset the mem and set the sha back to ready
+    dut.mem_ready.value = 0
+    dut.sha_ready.value = 1
+    dut._log.info("sha is set to ready")
+    #wait for rising edge to check
+    await RisingEdge(dut.clk)
+    assert dut.bus_ready.value == 1
+
+    #reset the sha and set the aes back to ready
+    dut.sha_ready.value = 0
+    dut.aes_ready.value = 1
+    dut._log.info("aes is set to ready")
+    #wait for rising edge to check
+    await RisingEdge(dut.clk)
+    assert dut.bus_ready.value == 1
+    
+    #set control to ready and aes back
+    dut.ctrl_ready.value = 1
+    dut.aes_ready.value = 0
+    dut._log.info("ctrl is set to ready")
+    #wait for rising edge and check
+    await RisingEdge(dut.clk)
+    assert dut.bus_ready.value == 1
+
+
+
+
