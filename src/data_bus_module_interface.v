874f853519418aa8b55b6c98bb6fe06e2937ec5b
Pengyu Ji <p6ji@uwaterloo.ca>
2026-02-05 13:15:37 -0500

    data_bus_module_interface v2

--- src/data_bus_module_interface.v
+++ src/data_bus_module_interface.v
@@ -1,42 +1,26 @@
 module data_bus_module_interface (
     // input from global arbiter
-    input [3:0] rd_grant,   
-    input [7:0] data_in,
+    input rd_grant,   
+    input [9:0] data_in, // [9]:ready, [8]:valid, [7:0]:data
 
     // output to the global arbiter
-    output reg [7:0] data_out,
-    output reg rdy_out,     // do we need rdy?
-    output reg valid_out,
+    output reg [9:0] data_out,
 
     // input from the module (e.g. from SHA, AES)
-    input [7:0] data_from_module,
-    input valid_from_module,
+    input [9:0] data_from_module,
 
     // output to the module
-    output reg [7:0] data_to_module,
-    output reg valid_to_module
+    output reg [9:0] data_to_module,
 );
 
 always @(*) begin
-    // defaults
-    valid_out = 1'b0;
-    valid_to_module = 1'b0;
 
-    data_to_module = data_in;
-
-    // if the rd_grant signal to a module is valid, 
-    // then the data send to the module is also valid
-    if (rd_grant[0] == 1'b1) begin
-        valid_to_module = 1'b1;
-    end
+    // if rd_grant is high, pass the data_in to the module
+    if (rd_grant) data_to_module = data_in;
+    // if rd_grant is low, manually change the valid bit of the data to 0
+    else data_to_module = {data_in[9], 1'b0, data_in[7:0]};
 
     data_out = data_from_module;
-
-    // if the data from module is valid, 
-    // then the data_out to the global arbiter is also valid
-    if (valid_from_module == 1'b1) begin
-        valid_out = 1'b1;
-    end
 end
 
 
